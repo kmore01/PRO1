9e00023dcf01dc74295da6494d5d0349713611de
kmore01 <marikevin7@gmail.com>
2025-06-22 23:48:06 +0200

    arreglo de cosas vacias

--- P9/P24730a.cpp
+++ P9/P24730a.cpp
@@ -1,8 +1,8 @@
 // program that prints the sorted list of the teachers from best to worst
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,26 +14,31 @@ struct Teacher {
 
 // function that orders teacher A and teacher B
 bool order(const Teacher &A, const Teacher &B) {
-    if (A.candies != B.candies) return A.candies > B.candies;
+    if (A.candies != B.candies)
+        return A.candies > B.candies;
     else {
-        if (A.carrots != B.carrots) return A.carrots > B.carrots;
+        if (A.carrots != B.carrots)
+            return A.carrots > B.carrots;
         else {
-            if (A.name.size() != B.name.size()) return A.name.size() < B.name.size();
-            else return A.name < B.name;
+            if (A.name.size() != B.name.size())
+                return A.name.size() < B.name.size();
+            else
+                return A.name < B.name;
         }
     }
 }
 
-int main () {
+int main() {
     int cases;
     cin >> cases;
     for (int k = 0; k < cases; ++k) {
         int numTeacher;
         cin >> numTeacher;
-        vector<Teacher> teachers(numTeacher); 
+        vector<Teacher> teachers(numTeacher);
         // we create a vector for all the teachers
         for (int i = 0; i < numTeacher; ++i) {
-            cin >> teachers[i].name >> teachers[i].candies >> teachers[i].carrots;
+            cin >> teachers[i].name >> teachers[i].candies >>
+                teachers[i].carrots;
         }
         // we order the vector to print it
         sort(teachers.begin(), teachers.end(), order);
--- P9/P30170.cpp
+++
@@ -1,4 +0,0 @@
-/* program that, given a sequence of words, computes how many characters have
-in total the words that appear once, how many characters have in total the
-words that appear twice, how many characters have in total the words that
-appear thrice, etc */
